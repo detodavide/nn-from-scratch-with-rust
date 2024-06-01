869e3b1e5c5f67a93db931de67a296813a2517dc
detodavide <davide.marchetti931@gmail.com>
2024-06-01 23:55:29 +0200

    add - neuron struct logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+extern crate ndarray;
+mod nn_elements;
+
+use ndarray::{Array1, Array2, array};
+use nn_elements::neuron::Neuron;
+
 fn main() {
-    println!("Hello, world!");
+    // Define an input vector of 3 dimensions
+    let inputs: Array1<f64> = array![1.0, 2.0, 3.0];
+
+    let neuron: Neuron = Neuron::new(3);
+
+    // Weights matrix to represent 3 neurons and 3 inputs -> 3x3
+    let weights: Array2<f64> = array![
+        [0.2, 0.8, 1.0],  //  each row represent a single neuron weights
+        [0.3, -0.67, 0.4], 
+        [1.0, 0.34, -0.4]
+    ];
+
+    // Biases for each neuron
+    let biases: Array1<f64> = array![0.8, 2.0, 3.0];
+
+    // Calculate the dot product and add the bias for each neuron
+    let neurons_no_bias: Array1<f64> = weights.dot(&inputs);
+    let neurons: Array1<f64> = neurons_no_bias + biases;
+
+    // Print the results
+    println!("Weights matrix:\n{:?}", weights);
+    println!("Output of neurons: {:?}", neurons);
 }
---
+++ src/nn_elements/neuron.rs
@@ -0,0 +1,19 @@
+use ndarray::Array1;
+use rand::Rng;
+
+pub struct Neuron {
+    pub weights: Array1<f64>,
+    pub bias: f64
+
+}
+
+impl Neuron {
+    // Method to create a new neuron with a given number of inputs
+    pub fn new(num_inputs: usize) -> Self {
+        let mut rng = rand::thread_rng();
+        let weights = Array1::from_shape_fn(num_inputs, |_| rng.gen_range(-1.0..1.0));
+        let bias = rng.gen_range(-1.0..1.0);
+
+        Neuron { weights, bias }
+    }
+}
